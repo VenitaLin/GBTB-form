7796481b6d085c23e8fce3221b8d607c22d1f1d8
Lin Chih Hsuan <chihhsuan.lin@SBF.ORG.SG>
2021-08-19 15:59:55 +0800

    first commit

---
+++ src/webparts/gbtbForm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGbtbFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GbtbFormWebPartStrings' {
+  const strings: IGbtbFormWebPartStrings;
+  export = strings;
+}
